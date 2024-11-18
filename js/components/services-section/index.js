bf358327bed920442c78d6c0ce2b757d8dbcdb2a
mashoun <mashoun.me@gmail.com>
2024-11-18 11:21:16 +0200

    +optimization in hero, services and founder

--- js/components/services-section/index.js
+++ js/components/services-section/index.js
@@ -6,21 +6,11 @@ export default {
         return {
             store,
             utilities,
-            breakpoints: {
-                768: {
-                    slidesPerView: 1,
-                    spaceBetween: 10,
-                },
-                1024: {
-                    slidesPerView: 4,
-                    spaceBetween: 10,
-                },
-            },
         }
     },
     computed: {
         filteredServices() {
-            return this.store.spinner ? ['','','','','','','','','',''] : this.store.services.filter(node => node.badge.trim().toUpperCase() != 'DEMO')
+            return this.store.spinner ? new Array(10) : this.store.services.filter(node => node.badge.trim().toUpperCase() != 'DEMO')
             // return this.store.services.filter(s => s.badge != 'DEMO')
             // return this.store.services
         }
